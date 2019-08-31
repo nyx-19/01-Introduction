6ff29c55007aed76b33d788cb105800a8994fa62
Caitlin Morton <caitlin.morton617@gmail.com>
2019-08-31 17:02:58 -0400

    finishes assignment

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('Hello, my name is Caitlin Morton.')
+print('My favorite game is the Dragon Age serise. We have been waiting forever for the 4th game since that betrayl in 3.')
+print('My concerns for this class is falling behind in the programming. I know nothing about any of this and I am a slow learner.')
+print('Despite not understanding, I am looking forward to eventually understanding it better. I want to be able to understand and communicate with programmers as an artist as well as handle some of the simpler programming work myself.')
+print('my stack overflow user number is 11980902')
+print('my github url is https://github.com/nyx-19')
